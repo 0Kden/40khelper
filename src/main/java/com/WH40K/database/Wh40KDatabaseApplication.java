afb2dd960660252ff4d1f337301bc95cef6f6379
Kaden <kgwyble232gmail.com>
2023-02-27 13:45:35 -0600

    Initial commit

---
+++ src/main/java/com/WH40K/database/Wh40KDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.WH40K.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wh40KDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wh40KDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WH40K/database/Wh40KDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WH40K.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wh40KDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
