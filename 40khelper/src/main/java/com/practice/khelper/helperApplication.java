5c6adb5dc6856302f14a53f821eab4f194aebc24
Kaden Wyble <kgwyble23@gmail.com>
2023-02-27 18:56:52 -0600

    Initial commit

---
+++ 40khelper/src/main/java/com/practice/khelper/helperApplication.java
@@ -0,0 +1,13 @@
+package com.practice.khelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class helperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(helperApplication.class, args);
+	}
+
+}
---
+++ 40khelper/src/test/java/com/practice/khelper/helperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.khelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class helperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
